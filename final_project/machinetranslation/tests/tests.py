b9cbd02c36dc501f49da513b1c837e33a4b6f109
Your Name <you@example.com>
2023-01-16 06:26:09 -0500

    added assertNotEqual and other functions

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -3,14 +3,22 @@ import unittest
 from translator import english_to_french, french_to_english
 
 class TestEnglishToFrench(unittest.TestCase):
+    """
+    This will test translation from english to french
+    """
     def test1(self):
-        self.assertEqual(english_to_french(''), ) # test when null is given as input.
+        self.assertEqual(english_to_french(''), '') # test when null is given as input.
         self.assertEqual(english_to_french('Hello'), 'Bonjour') # test when 'Hello' is given as input the output is 'Bonjour'.
+        self.assertNotEqual(english_to_french('Good morning'), 'Salut') # test when 'Hello' is given as input the output is not 'Hello'.
 
 class TestfrenchToEnglish(unittest.TestCase):
+    """
+    This will test translation from french to english
+    """
     def test1(self):
-        self.assertEqual(french_to_english(''), ) # test when null is given as input.
+        self.assertEqual(french_to_english(''), '') # test when null is given as input.
         self.assertEqual(french_to_english('Bonjour'), 'Hello') # test when 'Bonjour' is given as input the output is 'Hello'.
+        self.assertNotEqual(french_to_english('Salut'), 'Good morning') # test when 'Bonjour' is given as input the output is not 'Bonjour'.
 
-
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,5 +1,7 @@
-""" translator file that uses ibm watson api to translate different languages """
-# import json
+"""
+translator file that uses ibm watson api to translate different languages
+"""
+import json
 import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
@@ -13,18 +15,30 @@ url = os.environ['url']
 # Authenticator
 authenticator = IAMAuthenticator( apikey )
 # setup service
-lt = LanguageTranslatorV3(version = '2018-05-01', authenticator=authenticator)
-lt.set_service_url(url)
+language_translator = LanguageTranslatorV3(
+    version = '2018-05-01', 
+    authenticator=authenticator
+    )
+language_translator.set_service_url(url)
+language_translator.set_disable_ssl_verification(True)
 # Translating to French
 def english_to_french( english_text ):
-    #write the code here
-    """Function to translate english to french"""
-    french_text = lt.translate(text = english_text, model_id = 'en-fr').get_result()
-    return french_text
+    """
+    Function to translate english to french
+    """
+    french_text = language_translator.translate(
+        text = english_text, 
+        model_id = 'en-fr'
+        ).get_result()
+    return french_text.get("translations")[0].get("translation")
+
+# Translating to English
 def french_to_english( french_text ):
-    #write the code here
-    """Function to translate french to english"""
-    english_text = lt.translate(text = french_text, model_id = 'fr-en').get_result()
-    return english_text
-english_to_french('Hello World')
-french_to_english('Bonjour le monde')
+    """
+    Function to translate french to english
+    """
+    english_text = language_translator.translate(
+        text = french_text, 
+        model_id = 'fr-en'
+        ).get_result()
+    return english_text.get("translations")[0].get("translation")
